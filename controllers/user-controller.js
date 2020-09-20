315ab2a113633dc81de4d99dc866f8eb6e7d3db8
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-19 19:31:14 -0500

    user APIs are done

---
+++ controllers/user-controller.js
@@ -0,0 +1,78 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+          path: 'thoughts',
+          select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+          .then(dbData => res.json(dbData))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+          .then(dbData => {
+            if (!dbData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbData)
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+          .then(dbData => res.json(dbData))
+          .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true,  runValidators: true })
+          .then(dbData => {
+            if (!dbData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbData);
+          })
+          .catch(err => res.status(400).json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then(dbData => {
+            if (!dbData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbData);
+          })
+          .catch(err => res.status(400).json(err));
+    }
+     
+}
+
+module.exports = userController;
---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+//const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-routes');
+
+//router.use('/comments', commentRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
